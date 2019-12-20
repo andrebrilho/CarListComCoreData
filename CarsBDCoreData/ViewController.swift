aecf5b05e3a956d90e846e292d37d76753d4d4fb
andrebrilho <andre.txt@hotmail.com>
2019-12-20 14:18:30 -0300

    Initial Commit

---
+++ CarsBDCoreData/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CarsBDCoreData
+//
+//  Created by André Brilho on 20/12/19.
+//  Copyright © 2019 André Brilho. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
